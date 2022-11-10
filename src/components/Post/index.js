c807232556c89a659f1c5e092ec1ce93037f8964
jasonchalangary <jason42@tuta.io>
2022-11-10 17:41:27 +0000

    Finished task 5

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -2,8 +2,27 @@ import React from 'react';
 
 import './index.css';
 
-function Post() {
-  return <article></article>;
+
+function Post(props) {
+  return <article >
+
+      <h2>{props.title}</h2>
+      <p>{props.date}</p>
+      <p>{props.author}</p>
+      <p>{props.text}</p>
+      
+      
+      <img width= "75%" src ={props.image.link} alt = {props.image.alt}/>
+      <ul>
+      {
+        props.highlights.map((highlight) => {
+          console.log(highlight)
+          return <li key = {highlight.key}>{highlight.text}</li>
+        })
+      }
+      </ul>
+
+  </article>;
 }
 
 export default Post;
